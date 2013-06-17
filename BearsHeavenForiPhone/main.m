e927e14c8eb1de24a5ab6e3339c916303f03fe2f
ryonext <ryonext.s@gmail.com>
2013-06-17 23:37:49 +0900

    Initial Commit

---
+++ BearsHeavenForiPhone/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BearsHeavenForiPhone
+//
+//  Created by ryonext on 2013/06/17.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
